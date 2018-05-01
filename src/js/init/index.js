af091c0ff121fa89e9f3304be2a1dcd7fc9ec83b
yoichi kobayashi <info@tplh.net>
2018-05-01 23:53:40 +0900

    added code in the tutorial of tensorflow.js up to the chapter "Variables".

--- src/js/init/index.js
+++ src/js/init/index.js
@@ -1,6 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
 
 export default function() {
+  // Getting Start
   const model = tf.sequential();
   model.add(tf.layers.dense({
     units: 1,
@@ -14,5 +15,27 @@ export default function() {
   const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
   model.fit(xs, ys).then(() => {
     model.predict(tf.tensor2d([5], [1, 1])).print();
-  })
+  });
+
+  // Core Concepts in TensorFlow.js
+  const shape = [2, 3]; // 2 rows, 3 columns
+  const a = tf.tensor([1, 2, 3, 10, 20, 30], shape);
+  a.print();
+
+  const b = tf.tensor([[1, 2, 3], [10, 20, 30]]);
+  b.print();
+
+  const c = tf.tensor2d([[1, 2, 3], [10, 20, 30]]);
+  c.print();
+
+  const zeros = tf.zeros([3, 5]);
+  zeros.print();
+
+  const initVals = tf.zeros([5]);
+  const biases = tf.variable(initVals);
+  biases.print();
+
+  const updateVals = tf.tensor1d([0, 1, 0, 1, 0]);
+  biases.assign(updateVals);
+  biases.print();
 };
